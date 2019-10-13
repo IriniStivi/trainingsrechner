24fe318b2d2508d6b65a034b10bd69d4fd342a2a
Irini <irini.stivaktakis@googlemail.com>
2019-10-13 17:45:28 +0200

    Add files via upload

---
+++ ILB_Gewichtstabelle.py
@@ -0,0 +1,6 @@
+#round(x*blubb,2) = Ergebnisse auf zwei Stellen gerundet 
+
+
+for x in range(7,70,1):
+    print(x,"kg: 70%= ",round(x*0.7,2),"kg"," | ","80%= ",round(x*0.8,2),"kg"" | ","90%= ",round(x*0.9,2),"kg")
+    
